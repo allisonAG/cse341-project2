47c6e2e1f87f3e0baf935cd055f69af557282846
allisonAG <alli.aguirre95@gmail.com>
2026-01-21 17:55:56 -0600

    Error handling

--- controllers/books.js
+++ controllers/books.js
@@ -3,82 +3,101 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     //#swagger.tags=['Books']
-    const result = mongodb.getDatabase().db().collection('books').find();
-    result.toArray().then((books) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(books);
-    });
+    try {
+        const result = await mongodb.getDatabase().db().collection('books').find().toArray()
+            
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json('Some error occurred while retrieving books')
+    }
+    
 };
 
 const getSingle = async (req, res) => {
     //#swagger.tags=['Books']
-    if (!ObjectId.isValid(req.params.id)) {
-        return res.status(400).json('Invalid book id');
-    }
-    const book = await mongodb.getDatabase().db().collection('books').findOne({ _id: new ObjectId(req.params.id) });
-    if (!book) {
-        return res.status(404).json('Book not found');
-    }
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            return res.status(400).json('Invalid book id');
+        }
+
+        const book = await mongodb.getDatabase().db().collection('books').findOne({ _id: new ObjectId(req.params.id) });
+        
+        if (!book) {
+            return res.status(404).json('Book not found');
+        }
+        
         res.status(200).json(book);
-    
+    } catch (error) {
+        res.status(500).json('An error occurred while retrieving the book')
+    }
 };
 
 const createBook = async (req, res) => {
     //#swagger.tags=['Books']
-    const book = {
-        title: req.body.title,
-        author: req.body.author,
-        isbn: req.body.isbn,
-        genre: req.body.genre,
-        pages: Number(req.body.pages),
-        publishedYear: Number(req.body.publishedYear),
-        available: Boolean(req.body.available)
-    };
+    try {
+        const book = {
+            title: req.body.title,
+            author: req.body.author,
+            isbn: req.body.isbn,
+            genre: req.body.genre,
+            pages: Number(req.body.pages),
+            publishedYear: Number(req.body.publishedYear),
+            available: Boolean(req.body.available)
+        };
 
-    const response = await mongodb.getDatabase().db().collection('books').insertOne(book);
-    if (response.acknowledged) {
+        const response = await mongodb.getDatabase().db().collection('books').insertOne(book);
+
         res.status(201).json({ id: response.insertedId });
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while creating the book')
+    } catch (error) {
+        res.status(500).json('Some error occurred while creating the book')
     }
 };
 
-
 const updateBook = async (req, res) => {
     //#swagger.tags=['Books']
-    if (!ObjectId.isValid(req.params.id)) {
-        return res.status(400).json('Invalid book id');
-    }
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            return res.status(400).json('Invalid book id');
+        }
 
-    const book = {
-        title: req.body.title,
-        author: req.body.author,
-        isbn: req.body.isbn,
-        genre: req.body.genre,
-        pages: Number(req.body.pages),
-        publishedYear: Number(req.body.publishedYear),
-        available: Boolean(req.body.available)
-    };
+        const book = {
+            title: req.body.title,
+            author: req.body.author,
+            isbn: req.body.isbn,
+            genre: req.body.genre,
+            pages: Number(req.body.pages),
+            publishedYear: Number(req.body.publishedYear),
+            available: Boolean(req.body.available)
+        };
 
-    const response = await mongodb.getDatabase().db().collection('books').replaceOne({ _id: new ObjectId(req.params.id) }, book);
-    if (response.modifiedCount > 0) {
+        const response = await mongodb.getDatabase().db().collection('books').replaceOne({ _id: new ObjectId(req.params.id) }, book);
+    
+        if (response.matchedCount === 0) {
+            return res.status(404).json('Book not found');
+        }
+
         res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the book')
-    }
+    } catch (error) {
+        res.status(500).json('Some error occurred while updating the book')
+    } 
 };
 
 const deleteBook = async (req, res) => {
     //#swagger.tags=['Books']
-    if (!ObjectId.isValid(req.params.id)) {
-        return res.status(400).json('Invalid user id');
-    }
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            return res.status(400).json('Invalid user id');
+        }
 
-    const response = await mongodb.getDatabase().db().collection('books').deleteOne({ _id: new ObjectId(req.params.id) });
-    if (response.deletedCount > 0) {
+        const response = await mongodb.getDatabase().db().collection('books').deleteOne({ _id: new ObjectId(req.params.id) });
+
+        if (response.deletedCount === 0) {
+            return res.status(404).json('Book not found');
+        }
+
         res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the book')
+    } catch(error) {
+        res.status(500).json('Some error occurred while deleting the book')
     }
 };
 
--- controllers/users.js
+++ controllers/users.js
@@ -3,30 +3,40 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     //#swagger.tags=['Users']
-    const result = mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
-    });
+    try {
+        const result = await mongodb.getDatabase().db().collection('users').find().toArray()
+                    
+        res.status(200).json(result);
+    }
+    catch (error) {
+        res.status(500).json('Some error occurred while retrieving users')
+    }
 };
 
 const getSingle = async (req, res) => {
     //#swagger.tags=['Users']
-    
-    if (!ObjectId.isValid(req.params.id)) {
-        return res.status(400).json('Invalid user id');
-    }
-
-    const user = await mongodb.getDatabase().db().collection('users').findOne({ _id: new ObjectId(req.params.id) });
-    if (!user) {
-        return res.status(404).json('User not found');
-    }
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            return res.status(400).json('Invalid user id');
+        }
+        
+        const user = await mongodb.getDatabase().db().collection('users').findOne({ _id: new ObjectId(req.params.id) });
+                
+        if (!user) {
+            return res.status(404).json('User not found');
+        }
+                
         res.status(200).json(user);
+    }
+    catch (error) {
+        res.status(500).json('An error occurred while retrieving the user')
+    }
 };
 
 const createUser = async (req, res) => {
     //#swagger.tags=['Users']
-    const user = {
+    try {
+        const user = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
@@ -37,19 +47,19 @@ const createUser = async (req, res) => {
         createdAt: req.body.createdAt
     };
 
-    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
-    if (response.acknowledged) {
+        const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
+        
         res.status(201).json({ id: response.insertedId });
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while creating the user')
+    } catch (error) {
+        res.status(500).json('Some error occurred while creating the user')
     }
 };
 
 
 const updateUser = async (req, res) => {
     //#swagger.tags=['Users']
-    
-    if (!ObjectId.isValid(req.params.id)) {
+    try {
+         if (!ObjectId.isValid(req.params.id)) {
         return res.status(400).json('Invalid user id');
     }
     
@@ -64,26 +74,35 @@ const updateUser = async (req, res) => {
         createdAt: req.body.createdAt
     };
 
-    const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id: new ObjectId(req.params.id) }, user);
-    if (response.modifiedCount > 0) {
+        const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id: new ObjectId(req.params.id) }, user);
+        
+        if (response.matchedCount === 0) {
+            return res.status(404).json('User not found');
+        }
+    
         res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the user')
+    } catch(error) {
+        res.status(500).json('Some error occurred while updating the user')
     }
 };
 
 const deleteUser = async (req, res) => {
     //#swagger.tags=['Users']
-    
-    if (!ObjectId.isValid(req.params.id)) {
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
         return res.status(400).json('Invalid user id');
     }
     const response = await mongodb.getDatabase().db().collection('users').deleteOne({ _id: new ObjectId(req.params.id) });
-    if (response.deletedCount > 0) {
+    
+    if (response.deletedCount === 0) {
+            return res.status(404).json('User not found');
+        }
+
         res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the user')
+    } catch(error) {
+        res.status(500).json('Some error occurred while deleting the user')
     }
+    
 };
 
 
